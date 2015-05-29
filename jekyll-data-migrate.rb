047a552c5ea8a75912c7857e132195b9839f206e
Greg Boone <gregory.boone@gsa.gov>
2015-05-29 13:49:54 -0400

    Test of jekyll's command running abilities

---
+++ jekyll-data-migrate.rb
@@ -0,0 +1,20 @@
+require 'Jekyll'
+Class DataMigrate < Jekyll::Command
+  class << self
+    def init_with_program(prog)
+      prog.command(:migrate) do |c|
+        c.syntax "migrate [options]"
+        c.description 'Migrate values from data files to collection documents'
+        c.option 'from', '-f FROM', 'The data file to read from.'
+        c.option 'to', '-t TO', 'The targeted collection.'
+        c.action do |args, options|
+          self.echo(options)
+        end
+      end
+    def echo(opts)
+      puts opts['from']
+      puts opts['to']
+    end
+    end
+  end
+end
